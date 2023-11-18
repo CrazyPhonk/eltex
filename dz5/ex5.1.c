b93d8153e13ecb4ee25b0e5861cda54e3e7c9208
CrazyPhonk <barkhatov.egor@yandex.ru>
2023-11-18 22:31:33 +0700

    dz 5 done

---
+++ dz5/ex5.1.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 256
+
+int main(int argc, char *argv[]) {
+    char login[MAX_LENGTH];
+    char buf[MAX_LENGTH];
+    char symb;
+    char *line = NULL;
+    char readed;
+    size_t len = 0;
+    printf("Login:\n");
+    scanf("%s", login);
+    FILE * fileFd;
+    fileFd = fopen("/etc/passwd", "r");
+    if (!fileFd){
+        perror("Can not open the file\n");
+        exit(-1);
+    }
+     int i = 0; 
+    
+    while (fgets(buf, MAX_LENGTH, fileFd) != NULL) {
+        if(strstr(buf, login) != 0)
+        printf("%s", buf);
+        }
+}
